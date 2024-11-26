7b82d44810bcb444d29159f4ef56da1d93fb89b7
Jonis vitor <162247232+Jhonjonis@users.noreply.github.com>
2024-11-26 20:07:05 -0300

    Primeiro_comit

---
+++ script.js/scripts.js
@@ -0,0 +1,31 @@
+function atualizarContador() {
+    const dataAlvo = new Date("2025-12-31T23:59:59Z"); // Data alvo para o contador
+    const agora = new Date();
+
+    const tempoRestante = dataAlvo - agora;
+
+    if (tempoRestante <= 0) {
+      clearInterval(intervalo);
+      return; // Se o contador chegar ao fim, para a atualização
+    }
+
+    const anos = Math.floor(tempoRestante / (1000 * 60 * 60 * 24 * 365)); // Anos
+    const dias = Math.floor((tempoRestante % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24)); // Dias
+    const horas = Math.floor((tempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Horas
+    const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60)); // Minutos
+    const segundos = Math.floor((tempoRestante % (1000 * 60)) / 1000); // Segundos
+
+    document.getElementById("anos").textContent = anos;
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+  }
+
+  const intervalo = setInterval(atualizarContador, 1000); // Atualiza o contador a cada segundo
+
+
+    // Função para garantir que o áudio toque automaticamente ao carregar a página
+    window.onload = function() {
+      var audio = document.getElementById("audio");
+      audio.play();  // Tenta reproduzir a música automaticamente
+    };
